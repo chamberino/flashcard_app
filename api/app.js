953da5b861864f39bce4b158d58bf23348a3b4a2
cliftownh <cliftonhefley@gmail.com>
2019-12-07 14:10:19 -0800

    deploy test

--- api/app.js
+++ api/app.js
@@ -58,6 +58,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', catalogRouter);  // Adds catalog routes to middleware chain.
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

3a2498afef4fc7adf86db67a01f72558edd38a00
cliftownh <cliftonhefley@gmail.com>
2019-12-07 17:41:40 -0800

    static moved to www

--- api/app.js
+++ api/app.js
@@ -59,14 +59,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', catalogRouter);  // Adds catalog routes to middleware chain.
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('../client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('../client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//   });
+// }
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
